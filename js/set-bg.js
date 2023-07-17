9d2ce75cf41e1e73b76099cefd79dadba7ad11e7
MinhThuanNTM <139675840+MinhThuanNTM@users.noreply.github.com>
2023-07-17 15:19:18 +0700

    Delete js directory

--- js/set-bg.js
+++
@@ -1,6 +0,0 @@
-const bg = document.getElementsByClassName("set-bg");
-for (let i = 0; i < bg.length; i++) {
-    let data = bg[i].getAttribute("data-bg")
-    bg[i].style.backgroundImage = "url(./asset/img/product/"+ data +")"
-}
-
