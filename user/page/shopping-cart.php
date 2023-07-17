43b960463250da65f240b6e0e9377c3ad8c0a4ab
MinhThuanNTM <novalavie@gmail.com>
2023-07-17 16:37:43 +0700

    giao diện giỏ hàng

---
+++ user/page/shopping-cart.php
@@ -0,0 +1,164 @@
+    <link rel="stylesheet" href="../css/shopping-cart.css">
+    <div class="cart-page container">
+        <div class="row justify-content-between">
+            <div class="cart d-flex flex-column col-8">
+                <table class="table">
+                    <thead>
+                      <tr class="content-box">
+                        <th scope="col">
+                            <div class="cart-tile cart-item-width">
+                                Sản phẩm
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div class="cart-tile">
+                                số lượng
+                            </div>
+                        </th>
+                        <th scope="col">
+                            <div class="cart-tile">
+                                Tổng
+                            </div>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                            <div class="cart-item d-flex ">
+                                <div class="cart-prd-img-pd">
+                                    <div class="cart-prd-img set-bg" data-bg="00038-3896515113.png" >
+                                    </div>
+                                </div>
+                                <div class="cart-text content-box d-flex flex-column justify-content-center">
+                                    <a class="cart-prd-name">Đầm voan trắng</a>
+                                    <a class="cart-prd-price">1.240.000</a>
+                                </div> 
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity content-box d-flex justify-content-between align-items-center">
+                                <i class="fa-solid fa-angle-left"></i>
+                                <input type="text" value="0">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-total content-box d-flex align-items-center">
+                                <a>1.240.000</a>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-del content-box d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-xmark"></i>
+                            </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <div class="cart-item d-flex ">
+                                <div class="cart-prd-img-pd">
+                                    <div class="cart-prd-img set-bg" data-bg="00038-3896515113.png" >
+                                    </div>
+                                </div>
+                                <div class="cart-text content-box d-flex flex-column justify-content-center">
+                                    <a class="cart-prd-name">Đầm voan trắng</a>
+                                    <a class="cart-prd-price">1.240.000</a>
+                                </div> 
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity content-box d-flex justify-content-between align-items-center">
+                                <i class="fa-solid fa-angle-left"></i>
+                                <input type="text" value="0">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-total content-box d-flex align-items-center">
+                                <a>1.240.000</a>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-del content-box d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-xmark"></i>
+                            </div>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                            <div class="cart-item d-flex ">
+                                <div class="cart-prd-img-pd">
+                                    <div class="cart-prd-img set-bg" data-bg="00038-3896515113.png" >
+                                    </div>
+                                </div>
+                                <div class="cart-text content-box d-flex flex-column justify-content-center">
+                                    <a class="cart-prd-name">Đầm voan trắng</a>
+                                    <a class="cart-prd-price">1.240.000</a>
+                                </div> 
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity content-box d-flex justify-content-between align-items-center">
+                                <i class="fa-solid fa-angle-left"></i>
+                                <input type="text" value="0">
+                                <i class="fa-solid fa-angle-right"></i>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-total content-box d-flex align-items-center">
+                                <a>1.240.000</a>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="cart-prd-del content-box d-flex justify-content-center align-items-center">
+                                <i class="fa-solid fa-xmark"></i>
+                            </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <button class="continue-btn">
+                    Tiếp tục mua hàng
+                  </button>
+            </div>
+            <div class="col-3">
+                <div class="cart-calc d-flex flex-column">
+                    <div class="coupon-use d-flex flex-column">
+                        <a>mã giảm giá</a>
+                        <div class="coupon-code d-flex justify-content-between">
+                            <input type="text">
+                            <button class="coupon-apply-btn">dùng</button>
+                        </div>
+                    </div>
+                    <div class="price-total">
+                        <a class="calc-title">Tổng giá</a>
+                        <div class="cart-calc-text d-flex justify-content-between">
+                            <a>Tạm tính</a>
+                            <a>7.000.000 đ</a>
+                        </div>
+                        <div class="cart-calc-text  d-flex justify-content-between">
+                            <a >Giảm giá</a>
+                            <a>1.000 đ</a>
+                        </div>
+                        <div class="cart-calc-text d-flex justify-content-between">
+                            <a>Tổng cộng</a>
+                            <a >7.000.000 đ</a>
+                        </div>
+                        <button class="to-checkout-btn">đặt hàng</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+<script>
+    const bg = document.getElementsByClassName("set-bg");
+for (let i = 0; i < bg.length; i++) {
+    let data = bg[i].getAttribute("data-bg")
+    bg[i].style.backgroundImage = "url(./asset/img/product/"+ data +")"
+}
+
+
+</script>
