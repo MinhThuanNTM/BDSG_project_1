1d1e669a6582823baa38debecd8e518620a51eed
TruongTThinh <thinhtnpps26588@fpt.edu.vn>
2023-07-18 17:13:50 +0700

    update giao diện admin

--- admin/include/header.php
+++ admin/include/header.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <link rel="stylesheet" href="./css/style.css">
+
+    <link rel="stylesheet" href="./css/css copy.css">
+
     <title>Document</title>
 </head>
 <body>
