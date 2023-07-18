c482a55036830cbe5134f55b3a18b53030be3bea
Ngotrungtin1007 <tincho100700@gmail.com>
2023-07-18 16:31:52 +0700

    shop

---
+++ user/page/shop.php
@@ -0,0 +1,73 @@
+ <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="index.html"
+              ><img src="img/logoback.png" alt="" width="118px" height="32px"
+            /></a>
+          </li>
+        </ul>
+      </nav>
+      <div class="menu">
+        <ul>
+          <li>
+            <a href="index.html">Trang Chủ</a>
+            <a href="">/</a>
+            <a href="">Thời trang nam</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main>
+      <div class="banner">
+        <h3>Thời Trang Nam</h3>
+      </div>
+      <div class="h1">
+        <h1>Thời Trang Nam</h1>
+      </div>
+      <div class="dmuc">
+        <div class="h3">
+          <h3>Danh Mục</h3>
+        </div>
+        <div class="aonam"><input type="radio" name="#" /> Áo Nam</div>
+        <div class="quannam"><input type="radio" name="#" /> Quần Nam</div>
+      </div>
+      <div class="product-cha">
+        <div class="product-left">
+          <div class="menu-row">
+            <ul>
+              <li>
+                <a href="#"> Thiết Kế</a>
+              </li>
+              <li>
+                <a href="#"> Giới Tính</a>
+              </li>
+
+              <li>
+                <a href="#">Nhóm Sản Phẩm</a>
+              </li>
+              <li>
+                <a href="#">Kiểu Tay</a>
+              </li>
+
+              <li>
+                <a href="#">Màu Sắc</a>
+              </li>
+              <li>
+                <a href="#">Size</a>
+              </li>
+              <li>
+                <a href="#"> Giá Tiền</a>
+              </li>
+              <li>
+                <a href="#">Form</a>
+              </li>
+              <li>
+                <a href="#">Chất Liệu</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="product-right"></div>
+      </div>
+    </main>
