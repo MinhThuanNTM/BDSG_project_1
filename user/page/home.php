fbb08816a2c9883ae56c102683335bd06bfc0327
Shinnneva <110461189+nampvblacklotus@users.noreply.github.com>
2023-07-19 16:19:30 +0700

    add function

--- user/page/home.php
+++ user/page/home.php
@@ -1,236 +1,185 @@
-
-    <main>
-        <div class="banner">
-            <h3>Quần áo</h3>
-        </div>
-        <div class="introduce">
-            <div class="con1">
-                <img src="img/anh1.png" alt="Giới thiệu chung" width="380px" height="600px">
-                <div class="btn">
-                    <button>Xem Ngay</button>
-                </div>
-            </div>
-            <div class="con2">
-                <img src="img/anh2.jpg" alt="Giới Thiệu Chung" width="380px" height="600px">
-                <div class="btn">
-                    <button>Xem Ngay</button>
-                </div>
-            </div>
-            <div class="con3">
-                <img src="img/anh5.jpg" alt="Giới Thiệu Chung" width="380px" height="600px">
-                <div class="btn">
-                    <button>Xem Ngay</button>
-                </div>
+<main>
+    <div class="banner">
+        <h3>Quần áo</h3>
+    </div>
+    <div class="introduce">
+        <div class="con1">
+            <img src="img/anh1.png" alt="Giới thiệu chung" width="380px" height="600px">
+            <div class="btn">
+                <button>Xem Ngay</button>
             </div>
         </div>
-        <div class="banner1">
-
-            <div class="iframe">
-                <iframe width="700" height="400" src="https://www.youtube.com/embed/1gTvEIpl8K0"
-                    title="YouTube video player" frameborder="0"
-                    allow="controls; autoplay=1 ; muted; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen>
-                </iframe>
-
+        <div class="con2">
+            <img src="img/anh2.jpg" alt="Giới Thiệu Chung" width="380px" height="600px">
+            <div class="btn">
+                <button>Xem Ngay</button>
             </div>
         </div>
-        <div class="h1">
-            <h1>SẢN PHẨM MỚI</h1>
+        <div class="con3">
+            <img src="img/anh5.jpg" alt="Giới Thiệu Chung" width="380px" height="600px">
+            <div class="btn">
+                <button>Xem Ngay</button>
+            </div>
         </div>
-        <div class="ig"><img src="img/Screenshot 2023-06-02 124959.png" alt=""></div>
-        <di v class="product">
+    </div>
+    <div class="banner1">
 
+        <div class="iframe">
+            <iframe width="700" height="400" src="https://www.youtube.com/embed/1gTvEIpl8K0" title="YouTube video player" frameborder="0" allow="controls; autoplay=1 ; muted; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+            </iframe>
+
+        </div>
+    </div>
+    <div class="h1">
+        <h1>SẢN PHẨM MỚI</h1>
+    </div>
+    <div class="ig"><img src="img/Screenshot 2023-06-02 124959.png" alt=""></div>
+    <div class="product">
+        <?php
+        $limitedProducts = array_slice($product_list, 0, 4);
+        foreach ($limitedProducts as $product) {
+            $product_id = $product['product_id'];
+
+        ?>
             <div class="products1">
                 <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
+                    <img src="../user/img/icon1.png" alt="" width="58px" height="58px">
                 </div>
                 <div class="imgapso ">
-                    <img src="img/icon2.png" alt="" width="58px" height="35px">
+                    <img src="../user/img/icon2.png" alt="" width="58px" height="35px">
                 </div>
-                <div class="img"><img onmouseover="this.src='img/b1.1.png'" onmouseout="this.src='img/b1.jpg'"
-                        src="img/b1.jpg" alt="" width="280" height="380" /></div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>299.000 đ</span>
+                <div class="img"><img onmouseover="this.src='../user/img/<?php echo $product_img[$product_id]['img_2'] ?>'" onmouseout="this.src='../user/img/<?php echo $product_img[$product_id]['img_1'] ?>'" src="../user/img/<?php echo $img_url[$product_id]['img_3'] ?>" alt="" width="280" height="380" /></div>
+                <p><?php echo $product['name'] ?> -</p>
+                <span><?php echo $product['price'] ?> đ</span>
                 <div class="contai">
                     <a href="chitietsp.html">XEM THÔNG TIN CHI TIẾT</a>
                 </div>
 
             </div>
-            <div class="products2">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="img">
-                    <div class="img"><img onmouseover="this.src='img/b2.2.jpg'" onmouseout="this.src='img/b2.jpg'"
-                            src="img/b2.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>229.000 đ</span>
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
+        <?php } ?>
 
-            </div>
-            <div class="products3">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon2.png" alt="" width="58px" height="35px">
-                </div>
-                <div class="img">
 
-                    <div class="img"><img onmouseover="this.src='img/b3.3.jpg'" onmouseout="this.src='img/b3.jpg'"
-                            src="img/b3.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>229.000 đ</span>
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
+    </div>
+    <div class="h1">
+        <h1>ưu đãi đồng giá</h1>
+    </div>
+    <div class="ig"><img src="img/Screenshot 2023-06-02 124959.png" alt=""></div>
+    <div class="product">
+
+        <div class="products1">
+            <div class="imgposi">
+                <img src="img/icon1.png" alt="" width="58px" height="58px">
             </div>
-            <div class="products4">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon2.png" alt="" width="58px" height="35px">
-                </div>
-                <div class="img">
-                    <div class="img"><img onmouseover="this.src='img/b4.4.jpg'" onmouseout="this.src='img/b4.jpg'"
-                            src="img/b4.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>229.000 đ</span>
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
+            <div class="imgapso ">
+                <img src="img/icon4.png" alt="" width="58px" height="60px">
             </div>
-        </di>
-        <div class="h1">
-            <h1>ưu đãi đồng giá</h1>
+            <div class="img"><img onmouseover="this.src='img/c2.jpg'" onmouseout="this.src='img/c1.jpg'" src="img/c1.jpg" alt="" width="280" height="380" /></div>
+            <p>
+                Áo Sơ Mi Kaki Nam Denim Tay Ngắn Họa Tiết Form Loose - 10S21DSH001</p>
+            <span>229.000 đ </span>
+            <p class="discout">400.000đ</p>
+            <p class="quanty">-15%</p>
+
+            <div class="contai">
+                <a href="#">XEM THÔNG TIN CHI TIẾT</a>
+            </div>
+
         </div>
-        <div class="ig"><img src="img/Screenshot 2023-06-02 124959.png" alt=""></div>
-        <div class="product">
-
-            <div class="products1">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon4.png" alt="" width="58px" height="60px">
-                </div>
-                <div class="img"><img onmouseover="this.src='img/c2.jpg'" onmouseout="this.src='img/c1.jpg'"
-                        src="img/c1.jpg" alt="" width="280" height="380" /></div>
-                <p>
-                    Áo Sơ Mi Kaki Nam Denim Tay Ngắn Họa Tiết Form Loose - 10S21DSH001</p>
-                <span>229.000 đ </span>
-                <p class="discout">400.000đ</p>
-                <p class="quanty">-15%</p>
-
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
-
+        <div class="products2">
+            <div class="imgposi">
+                <img src="img/icon1.png" alt="" width="58px" height="58px">
             </div>
-            <div class="products2">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon4.png" alt="" width="58px" height="60px">
-                </div>
-                <div class="img">
-                    <div class="img"><img onmouseover="this.src='img/3.jpg'" onmouseout="this.src='img/a3.jpg'"
-                            src="img/a3.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nữ Cúp Ngực Trơn Form Fitted Crop - 10S22TTOW007</p>
-                <span>229.000 đ</span>
-                <p class="discout">400.000đ</p>
-                <p class="quanty">-15%</p>
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
-
+            <div class="imgapso ">
+                <img src="img/icon4.png" alt="" width="58px" height="60px">
             </div>
-            <div class="products3">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon4.png" alt="" width="58px" height="60px">
-                </div>
-                <div class="img">
-
-                    <div class="img"><img onmouseover="this.src='img/c4.jpg'" onmouseout="this.src='img/c3.jpg'"
-                            src="img/c3.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>229.000 đ</span>
-                <p class="discout">400.000đ</p>
-                <p class="quanty">-15%</p>
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
+            <div class="img">
+                <div class="img"><img onmouseover="this.src='img/3.jpg'" onmouseout="this.src='img/a3.jpg'" src="img/a3.jpg" alt="" width="280" height="380" /></div>
             </div>
-            <div class="products4">
-                <div class="imgposi">
-                    <img src="img/icon1.png" alt="" width="58px" height="58px">
-                </div>
-                <div class="imgapso ">
-                    <img src="img/icon4.png" alt="" width="58px" height="60px">
-                </div>
-                <div class="img">
-                    <div class="img"><img onmouseover="this.src='img/c6.jpg'" onmouseout="this.src='img/c5.jpg'"
-                            src="img/c5.jpg" alt="" width="280" height="380" /></div>
-                </div>
-                <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
-                <span>229.000 đ</span>
-                <p class="discout">400.000đ</p>
-                <p class="quanty">-15%</p>
+            <p>Áo Thun Nữ Cúp Ngực Trơn Form Fitted Crop - 10S22TTOW007</p>
+            <span>229.000 đ</span>
+            <p class="discout">400.000đ</p>
+            <p class="quanty">-15%</p>
+            <div class="contai">
+                <a href="#">XEM THÔNG TIN CHI TIẾT</a>
+            </div>
 
-                <div class="contai">
-                    <a href="#">XEM THÔNG TIN CHI TIẾT</a>
-                </div>
+        </div>
+        <div class="products3">
+            <div class="imgposi">
+                <img src="img/icon1.png" alt="" width="58px" height="58px">
+            </div>
+            <div class="imgapso ">
+                <img src="img/icon4.png" alt="" width="58px" height="60px">
+            </div>
+            <div class="img">
+
+                <div class="img"><img onmouseover="this.src='img/c4.jpg'" onmouseout="this.src='img/c3.jpg'" src="img/c3.jpg" alt="" width="280" height="380" /></div>
+            </div>
+            <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
+            <span>229.000 đ</span>
+            <p class="discout">400.000đ</p>
+            <p class="quanty">-15%</p>
+            <div class="contai">
+                <a href="#">XEM THÔNG TIN CHI TIẾT</a>
             </div>
         </div>
-
-
-        <div class="container">
-
-            <div class="nam"><img src="img/a.png" alt="" width="609px" height="498px">
-                <span class="xem"><a href="product.html">XEM NGAY</a></span>
+        <div class="products4">
+            <div class="imgposi">
+                <img src="img/icon1.png" alt="" width="58px" height="58px">
             </div>
-            <div class="nu"><img src="img/4.4.jpg" alt="" width="609px" height="498px">
-                <span class="xem"><a href="productnu.html">XEM NGAY</a></span>
+            <div class="imgapso ">
+                <img src="img/icon4.png" alt="" width="58px" height="60px">
             </div>
-        </div>
+            <div class="img">
+                <div class="img"><img onmouseover="this.src='img/c6.jpg'" onmouseout="this.src='img/c5.jpg'" src="img/c5.jpg" alt="" width="280" height="380" /></div>
+            </div>
+            <p>Áo Thun Nam Tay Ngắn In Ngực Years Of Love Form Fitted - 10S23TSS080</p>
+            <span>229.000 đ</span>
+            <p class="discout">400.000đ</p>
+            <p class="quanty">-15%</p>
 
-        <div class="h1">
-            <h1>TIN THỜI TRANG</h1>
-        </div>
-        <div class="last">
-            <div class="imglast1">
-                <img src="img/1.jpg" alt="" width="396px" height="294px">
-                <div class="boder"><a href="#">New Collection</a> <a href="#">New Collection</a></div>
-                <div class="p">10 Years Of Love | Mix & Match Cùng Chiếc Áo Thun Mừng Sinh Nhật Routine 10 Tuổi</div>
-                <div class="day">June 19, 2023</div>
-
-            </div>
-            <div class="imglast2">
-                <img src="img/2.jpg" alt="" width="396px" height="294px">
-                <div class="boder"><a href="#">Sự Kiện</a> </div>
-                <div class="p">10 Years Of Love - Hành trình 10 năm với Thương hiệu Routine</div>
-                <div class="day">June 19, 2023</div>
-            </div>
-            <div class="imglast3">
-                <img src="img/11.jpg" alt="" width="396px" height="294px">
-                <div class="boder"><a href="#">Sự Kiện</a> </div>
-                <div class="p">Kết quả Minigame ĐUA TOP LẬT HÌNH - RINH QUÀ SINH NHẬT | Lộ diện chủ nhân chiếc điện
-                    thoại</div>
-                <div class="day">June 19, 2023</div>
+            <div class="contai">
+                <a href="#">XEM THÔNG TIN CHI TIẾT</a>
             </div>
         </div>
+    </div>
 
 
-    </main>
+    <div class="container">
+
+        <div class="nam"><img src="img/a.png" alt="" width="609px" height="498px">
+            <span class="xem"><a href="product.html">XEM NGAY</a></span>
+        </div>
+        <div class="nu"><img src="img/4.4.jpg" alt="" width="609px" height="498px">
+            <span class="xem"><a href="productnu.html">XEM NGAY</a></span>
+        </div>
+    </div>
+
+    <div class="h1">
+        <h1>TIN THỜI TRANG</h1>
+    </div>
+    <div class="last">
+        <div class="imglast1">
+            <img src="img/1.jpg" alt="" width="396px" height="294px">
+            <div class="boder"><a href="#">New Collection</a> <a href="#">New Collection</a></div>
+            <div class="p">10 Years Of Love | Mix & Match Cùng Chiếc Áo Thun Mừng Sinh Nhật Routine 10 Tuổi</div>
+            <div class="day">June 19, 2023</div>
+
+        </div>
+        <div class="imglast2">
+            <img src="img/2.jpg" alt="" width="396px" height="294px">
+            <div class="boder"><a href="#">Sự Kiện</a> </div>
+            <div class="p">10 Years Of Love - Hành trình 10 năm với Thương hiệu Routine</div>
+            <div class="day">June 19, 2023</div>
+        </div>
+        <div class="imglast3">
+            <img src="img/11.jpg" alt="" width="396px" height="294px">
+            <div class="boder"><a href="#">Sự Kiện</a> </div>
+            <div class="p">Kết quả Minigame ĐUA TOP LẬT HÌNH - RINH QUÀ SINH NHẬT | Lộ diện chủ nhân chiếc điện
+                thoại</div>
+            <div class="day">June 19, 2023</div>
+        </div>
+    </div>
+
+
+</main>
